271ce454e3e8775ff042f2269e1dcf6ab75afd13
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2025-08-27 01:03:25 -0400

    Add files via upload

--- livetv.py
+++ livetv.py
@@ -194,7 +194,11 @@ def create_playlist():
             for channel in channels:
                 # Write channel entry
                 # Format the channel name as 'NAME vCHANNEL.NUM' (e.g., 'WWAY ABC v10.1')
-                channel_name = f"{channel[1].split(' ')[0]} {channel[1].split(' ')[1]} v{channel[0]}"
+                name_parts = channel[1].split()
+                if len(name_parts) > 1:
+                    channel_name = f"{name_parts[0]} {name_parts[1]} v{channel[0]}"
+                else:
+                    channel_name = f"{channel[1]} v{channel[0]}"
                 f.write(f'#EXTINF:-1 tvg-id="{channel[0]}" tvg-name="{channel_name}",{channel_name}\n')
                 f.write(f'#EXTVLCOPT:http-referer={base_url}/\n')
                 f.write(f'#EXTVLCOPT:http-user-agent=Mozilla/5.0\n')
